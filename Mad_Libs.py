e90e9fc9c4a2ba0fa1eaa35cbf4e7895f2b49c52
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-02 12:34:50 -0700

    [add] completed the short story

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -1,10 +1,39 @@
 """
 Once upon a time in planet "planet", lived a daring 5 years old "noun1" named "Name1". For one weekend, "Name1" together with 
-his mother, father, and were invited to a huge gathering in a medium sized resort consisted of close friends, families, 
+"his/her" lovely mother, father, and two sibings were invited to a huge gathering in a medium sized resort consisted of close friends, families, 
 and some random "pronoun1". This resort had two "adjective1" swimming pools: one for young "noun1(plural)" that begins 
-from zero to two feet deep, and the other pool for older and taller "noun1(plural)" that begins from 4 feet all the way up 
+from zero to two feet deep, and another pool for the older and taller "noun1(plural)" that begins from 4 feet all the way up 
 to "big integer" feet deep. "Name1" not usually having many chances to swim in a swimming pool, had lots of fun with 
-other young "noun1 plural" playing tag. 
+other young "noun1 plural" playing tag. A few hours later as the night approached, "Name1" looked to "his/her" left at the other swimming
+pool and saw how the older "noun1(plural)" were having so much fun. "He/She" thought to himself, why not me? So "Name1" tipped toe past the gazebo
+full of people and made "his/her" way to the steps of the swimming pools. "Name1" carefuly took one step at a time while holding on tightly
+the wall of the pool. With a huge grin on "his/her" face, "he/she" reached the last step of the pool where the water only reached all the way up to "his/her" chest. Curious on how deep
+the last step was, "he/she" took one last step, which "he/she" found out was the tallest step that immediately put the water level just right above
+"Name1"'s eye level. "He/she" tried to grab the edge but it was too far, "he/she" tried to retrack "his/her" steps, but it was too far and high. Lastly,
+"he/she" tried to jump up to scream for help, but noone can hear him because "his/her" mouth was underneath the water. Jumping could only
+do so little as water still flows down from "his/her" hair down to "his/her" face, removing "his/her" ability to breathe through "his/her" nose. "He/She" was 
+however in peace to see everyone having fun, laughing, as the round-shape light of the lamp turns to diamond then to a straight line. 
+Is this the end for the the daring and energetic "noun1" from planet "planet"?
+
+A couple of minutes later, a "pronoun2" saw and decided to join the fun with other older "noun1" in the swimming pool. As he walks down the stairs
+of the pool, he stepped on a little "noun1" who fully sank underneath the pool. Not knowing or caring who it was, he immediately took the "noun1" 
+out of the pool and alerted everyone that someone drowned. Everyone had a curious and worried face and immediately rushed in to see who it was.
+They saw a young "noun1" with a huge belly from all the water inhaled, purple from head to toe, and worst of all, with no heartbeat. The father
+accompanied by a closed "pronoun1", immediately rushed to give his son a CPR. Not caring about the current repercussion and taste of the pool 
+water mixed with "flavor" flavor from the inside of a dead "noun1", the father kept pumping and giving all his oxygen to his dead son. A couple 
+of water removed and minutes passed, yet still no breathe nor heartbeat, but only discharges of waste from his rear bottom. After some prayers and 
+cries from the observing concerned "pronoun1(plural)", the young "noun1" coughed out the remaining water. Still unconscious but this time, "he/she" 
+was breathing and there was a heartbeat. 
+
+The next morning, "Name1" found "himself/herself" in a new place, everything was mostly white, it was a hospital "he/she" slept at for the night. 
+After a breakfast, "he/she" returned to the resort with the father, greeted everyone and noticed all eyes were glued on "him/her". Everyone 
+looked at "him/her" differently, as "he/she" did looked different. This little five years old "noun1" went face to face and was victorious 
+against a battle noone in the gathering has ever fought before nor dares to confront. "He/She" served as an insipration to everyone, that if 
+"he/she" overcame death itself, they can too. "He/She" raised many eyebrows on what else is "he/she" meant to do? What else has God planned for "him/her"? That is too incomprehensible to know.
+However, there is one thing "Name1" knows for sure, it is that "he/she" is meant to overcome any challenges "he/she" comes across. Now "Name1" is in
+Make School to not simply get a degree, but to initialize a positive wave and dominate. To lead and be the best possible example for anyone 
+"he/she" comes in contact with. Most importantly, to be unfazed and abide to what "he/she" believes is right. "His/her" curiousity will take him far,
+but "his/her" imagination and positive attitude will take him further.
 
 
 
@@ -13,8 +42,14 @@ other young "noun1 plural" playing tag.
 planet = Earth
 noun1 = kid
 name1 = Samuel
-pronoun1 = church friends
-adjective1 = clear/beautiful
+he/she = male
+him/her = him
+his/her = his
+himself/herself = male
+pronoun1 = church friends/people
+pronoun2 = angel/savior
+adjective1 = huge/clear/beautiful
 big integer = 6
+flavor = icky
 
 """
