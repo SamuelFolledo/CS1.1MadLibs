9a2b16eb1d82803193a5ccf19e3adb1fadbea201
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-02 00:44:04 -0700

    [add] added more on the story

---
+++ Mad_Libs.py
@@ -0,0 +1,20 @@
+"""
+Once upon a time in planet "planet", lived a daring 5 years old "noun1" named "Name1". For one weekend, "Name1" together with 
+his mother, father, and were invited to a huge gathering in a medium sized resort consisted of close friends, families, 
+and some random "pronoun1". This resort had two "adjective1" swimming pools: one for young "noun1(plural)" that begins 
+from zero to two feet deep, and the other pool for older and taller "noun1(plural)" that begins from 4 feet all the way up 
+to "big integer" feet deep. "Name1" not usually having many chances to swim in a swimming pool, had lots of fun with 
+other young "noun1 plural" playing tag. 
+
+
+
+
+
+planet = Earth
+noun1 = kid
+name1 = Samuel
+pronoun1 = church friends
+adjective1 = clear/beautiful
+big integer = 6
+
+"""
